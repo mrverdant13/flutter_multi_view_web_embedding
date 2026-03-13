976865fa59232ac2964b0be8eb7b08f997e5cafd
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2026-03-13 16:51:15 -0500

    refactor(flutter-bootstrap): use empty main (#14)

--- packages/flutter_bootstrap/lib/main.dart
+++ packages/flutter_bootstrap/lib/main.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/widgets.dart';
-
-void main() {
-  runApp(const _App());
-}
-
-class _App extends StatelessWidget {
-  const _App();
-
-  @override
-  Widget build(BuildContext context) {
-    return WidgetsApp(
-      color: const Color(0xFF000000),
-      builder: (_, __) => const SizedBox.shrink(),
-    );
-  }
-}
+void main() {}
